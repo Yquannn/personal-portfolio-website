16181d2bf9e52f05d30816cade661d0b36fcfe6a
Yquan <dondonbautista1223@gmail.com>
2024-03-03 18:59:15 +0800

    second commit

--- anmation.js
+++ anmation.js
@@ -1,6 +1,18 @@
 var typed = new Typed(".name",{
-  strings: ["Don-don Bautista", " A Programmer"],
+  strings: ["Dondon Bautista", " A Programmer"],
   typeSpeed : 150,
   backSpeed : 100,
   loop : true
 })
+
+const hamburger_menu = document.querySelector(".hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
+hamburger_menu.addEventListener("click", () => {
+  hamburger_menu.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".header-icon").forEach(n => n.
+  addEventListener("click", ()=>{
+    hamburger_menu.classList.remove('active');
+  }))
