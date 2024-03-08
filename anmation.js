10445c91e24a43578b01347402de877235e53f38
Yquan <dondonbautista1223@gmail.com>
2024-03-08 14:34:03 +0800

    third push

--- anmation.js
+++ anmation.js
@@ -7,13 +7,40 @@ var typed = new Typed(".name",{
 
 const hamburger_menu = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
+
 hamburger_menu.addEventListener("click", () => {
   hamburger_menu.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".header-icon").forEach(n => n.
-  addEventListener("click", ()=>{
-    hamburger_menu.classList.remove('active');
-  }))
+// Here you are selecting a single element, so no need to use forEach
+document.querySelector(".nav-menu").addEventListener("click", () => {
+  // When clicking the nav menu, remove the 'active' class from both hamburger menu and nav menu
+  hamburger_menu.classList.remove('active');
+  navMenu.classList.remove('active');
+});
+
+
+const btn = document.getElementById('button');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  btn.value = 'Sending...';
+
+  const serviceID = 'service_1e4fj8h';
+  const templateID = 'template_tox677s';
+
+  emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+      // Reset the form after successful submission
+      form.reset();
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
 
